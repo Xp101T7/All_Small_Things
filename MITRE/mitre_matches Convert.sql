7d0bd047ec6255e75d36fa1e110146ba30e980a6
Xp1017r <sansoscuridad@gmail.com>
2024-08-08 14:02:11 -0700

    push

--- MITRE/mitre_matches Convert.sql
+++ MITRE/mitre_matches Convert.sql
@@ -64,12 +64,11 @@
 
 | eval combined_mitre=mvappend(mitre, mitre_T1, mitre_TA)
 | eval combined_mitre=split(mvjoin(combined_mitre, ","), ",")
-| eval combined_mitre=mvfilter(match(combined_mitre, "^(?i)(T\d{1,4}(\.\d{1,3})?|TA\d{4}(\.\d{1,3})?)$"))
-| eval combined_mitre=mvmap(combined_mitre, upper(trim(combined_mitre)))
-| eval unique_mitre = [| stats values(combined_mitre) as combined_mitre by _raw | return $combined_mitre]
-| eval unique_mitre=mvsort(mvuniq(split(unique_mitre, ",")))
+| eval combined_mitre=mvfilter(match(combined_mitre, "^(?i)(T\d{1,4}(\.\d{1,3})?|TA\d{4}(\.\d{1,3})?)$")),
+    combined_mitre=mvmap(combined_mitre, upper(trim(combined_mitre)))
+| eventstats values(combined_mitre) as combined_mitre by _raw
 | eval annotations=if(
-    isnotnull(unique_mitre) AND mvcount(unique_mitre) > 0, 
-    "(\"mitre_attack\":[\"" + mvjoin(unique_mitre, "\", \"") + "\"])", 
+    isnotnull(combined_mitre) AND mvcount(combined_mitre) > 0, 
+    "(\"mitre_attack\":[\"" + mvjoin(mvuniq(combined_mitre), "\", \"") + "\"])", 
     null()
 )
